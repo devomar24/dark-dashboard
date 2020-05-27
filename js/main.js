a68e04567aa92cb8c1b578ab920d295a6987a618
omar yassir <omaryassir124@gmail.com>
2020-05-27 12:47:56 -0700

    dark dashboard ui

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  //toggle sidebar
+  $(document).ready(function () {
+    $("#sidebarCollapse").on("click", function () {
+      $("#sidebar").toggleClass("active");
+    });
+  });
+
+});
